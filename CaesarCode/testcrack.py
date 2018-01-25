35d803ee9f2ff61dd93948d0d2a13ca3b6469f40
CatLearned <16177346+CatLearned@users.noreply.github.com>
2018-01-25 22:41:37 +0300

    Дописан тест для оценки работы Частотного анализа, требуется рефакторинг

--- CaesarCode/testcrack.py
+++ CaesarCode/testcrack.py
@@ -1,8 +1,8 @@
 import modularArithmetic
+import random
 import frequencyAnalysis
 from Alphabets import alpEng
 from operator import attrgetter
-
 
 class CryptRes:
     def __init__(self, key, message, frqnc):
@@ -10,19 +10,25 @@ class CryptRes:
         self.message = message
         self.frqnc = frqnc
 
-
-print("Шифр Цезаря (Расшифрование)")
-mesg = input('-> Введите шифротекст: ')
+print("Шифр Цезаря (Шифрование-тест)")
+mesg = input('-> Введите сообщение: ')
 mesg = mesg.upper().replace(' ', '')
-
-slength = len(mesg)
+key = random.randint(1, alpEng.Length)
+print(alpEng.Dic[key])
+it = 0
+codedMsg = ''
+while not it == len(mesg):
+    scode = alpEng.Alp[mesg[it]]
+    rcode = modularArithmetic.modularsumm(scode, key, alpEng.Length)
+    codedMsg = codedMsg + alpEng.Dic[rcode]
+    it = it + 1
 keycode = 0
 decryptmsg = []
 while not keycode == alpEng.Length:
     it = 0
     res = ''
-    while not it == slength:
-        scode = alpEng.Alp[mesg[it]]
+    while not it == len(codedMsg):
+        scode = alpEng.Alp[codedMsg[it]]
         rcode = modularArithmetic.modulardec(scode, keycode, alpEng.Length)
         res = res + alpEng.Dic[rcode]
         it = it + 1
--- frequencyAnalysis.py
+++ frequencyAnalysis.py
@@ -8,22 +8,22 @@ def quadAnalysis(mesgArr):
         statsquad[quad] = int(count)
     itter = 0
     while itter < len(mesgArr):
-        mesg = mesgArr[itter].split(":")
-        if len(mesg[1]) < 4:
-            mesgArr[itter] = mesgArr[itter] + ':Анализ невозможен'
+        mesg = mesgArr[itter].message
+        if len(mesg) < 4:
+            mesgArr[itter].frqnc = ':Анализ невозможен'
         else:
             res = 0
             counter = 4
             summquad = sum(statsquad.values())
-            while counter <= len(mesgArr[itter]):
-                buff = mesg[1]
+            while counter <= len(mesg):
+                buff = mesg
                 buff = buff[counter - 4: counter]
                 if buff in statsquad:
                     res = res + log10(float(statsquad[buff])/summquad)
                 else:
                     res = res + log10(0.01 / summquad)
                 counter = counter + 1
-            mesgArr[itter] = mesgArr[itter] + ":" + str(int(res))
+            mesgArr[itter].frqnc = res
         itter = itter + 1
     f.close()
     return mesgArr
