dd9bc543bb948f2079ff1de9bbec859146ada6af
CatLearned <VovanVinni@gmail.ru>
2018-02-02 14:34:12 +0300

    Переделано Шифрование Цезаря в отдельную процедуру

--- CaesarCode/crypt.py
+++ CaesarCode/crypt.py
@@ -1,21 +1,38 @@
 import modularArithmetic
-from Alphabets import alpEng
+
+
+# from Alphabets import alpEng as ALPHABET
+
+# { "Eng" : from Alphabets import alpEng as ALPHABET}
+
+def CRYPTION_CEASAR(TEXT, LANGUAGE, KEY):
+    if (LANGUAGE == 'eng'):
+        from Alphabets import alpEng as ALPHABET
+    else:
+        return "Неверный алфавит!"
+    mesg = TEXT.upper().replace(' ', '')
+    key = KEY[0].upper()
+    try:
+        keycode = ALPHABET.Alp[key]
+    except:
+        return "Ошибка конвертации ключа, ключ отсутствует в словаре"
+    slength = len(mesg)
+    it = 0
+    res = ''
+    while not it == slength:
+        try:
+            scode = ALPHABET.Alp[mesg[it]]
+        except:
+            return "В строке ошибочный символ: " + "\"" + mesg[it] + "\""
+        rcode = modularArithmetic.modularsumm(scode, keycode, ALPHABET.Length)
+        res = res + ALPHABET.Dic[rcode]
+        it = it + 1
+    return res
+
 
 print("Шифр Цезаря (Шифрование)")
 mesg = input('-> Введите сообщение: ')
 mesg = mesg.upper().replace(' ', '')
 key = input('-> Ключ символ: ')
 key = key[0].upper()
-
-slength = len(mesg)
-keycode = alpEng.Alp[key]
-it = 0
-res = ''
-
-while not it == slength:
-    scode = alpEng.Alp[mesg[it]]
-    rcode = modularArithmetic.modularsumm(scode, keycode, alpEng.Length)
-    res = res + alpEng.Dic[rcode]
-    it = it + 1
-
-print("Зашифрованное сообщение: " + res)
+print("Зашифрованное сообщение: " + CRYPTION_CEASAR(mesg, key, 'eng'))
